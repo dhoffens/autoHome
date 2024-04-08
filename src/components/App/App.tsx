2bb939a1f3fd205c16e95b7899124a67bfa0d00b
Daniel Hoffens <dhoffens@gmail.com>
2024-04-08 19:52:33 -0400

    cleanup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Sidenav from '../Sidenav/Sidenav';
 
@@ -8,16 +7,7 @@ const App: React.FC = () => {
   return (
     <>
       <Header title='AutoHome' description='header description'/>
-      <Sidenav />
-      
-      <Router>
-        <Routes>
-          {/* / goes to Home page with will have dashboard, sidenav and about */}
-          <Route path="/" element={<h1>AutoHome</h1>} />
-          {/* each other route will go to one of the modules */}
-          <Route path="/about" element={<p>a tool for new homeowners</p>} />
-        </Routes>
-      </Router>
+      <Sidenav />      
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,6 @@
-// import React from 'react'
 import App from './components/App/App.tsx'
 import './index.css'
 import { createRoot } from 'react-dom/client'
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
 
 const container = document.getElementById('root')!
 const root = createRoot(container);
