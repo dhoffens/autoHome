ce17d1f4aa98f409c059da5a991d6435bc436a6d
Daniel Hoffens <dhoffens@gmail.com>
2024-04-10 13:24:25 -0400

    add react-big-calendar its rly big

--- src/components/MaintenanceSchedule/MaintenanceSchedule.tsx
+++ src/components/MaintenanceSchedule/MaintenanceSchedule.tsx
@@ -1,10 +1,34 @@
 import styles from './MaintenanceSchedule.module.scss';
 import React from 'react';
+import 'react-big-calendar/lib/sass/styles.scss';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = momentLocalizer(moment);
+
+const events = [
+  {
+    title: 'All Day Event',
+    start: new Date(2021, 1, 1),
+    end: new Date(2021, 1, 1),
+  },
+];
+
+const MyCalendar = () => (
+  <Calendar
+    localizer={localizer}
+    events={events}
+    startAccessor="start"
+    endAccessor="end"
+    style={{ height: 1000 }}
+  />
+)
 
 export const MaintenanceSchedule: React.FC = () => {
   return (
     <div>
       <h2 className={styles.title}>Maintenance Schedule</h2>
+      <MyCalendar />
     </div>
   );
 };
